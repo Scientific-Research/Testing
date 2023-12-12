78d1e6f6b9247634bd03f613710799be78b5b171
Maximilian <scientificresearch309@gmail.com>
2023-12-12 19:33:27 +0100

    Testing Asynchronous Code!

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -5,11 +5,24 @@ const User = require("../models/user");
 const AuthController = require("../controllers/auth");
 
 describe("Auth Controller - Login", function () {
-  it("should throw an error with code 500 if accessing the database fails!", function () {
+  it("should throw an error with code 500 if accessing the database fails!", function (done) {
     sinon.stub(User, "findOne");
     User.findOne.throws();
 
-    expect(AuthController.login);
+    const req = {
+      body: {
+        email: "test@test.com",
+        password: "123456",
+      },
+    };
+
+    // expect(AuthController.login);
+    AuthController.login(req, {}, () => {}).then((result) => {
+      //   console.log(result);
+      expect(result).to.be.an("error");
+      expect(result).to.have.property("statusCode", 500);
+      done();
+    });
 
     User.findOne.restore();
   });
