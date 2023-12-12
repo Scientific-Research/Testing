bde6cd87a049307524036008cbd44dc5c7681e5b
Maximilian <scientificresearch309@gmail.com>
2023-12-12 20:44:20 +0100

    Setting up a testing database

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -1,5 +1,6 @@
 const expect = require("chai").expect;
 const sinon = require("sinon");
+const mongoose = require("mongoose");
 
 const User = require("../models/user");
 const AuthController = require("../controllers/auth");
@@ -25,5 +26,31 @@ describe("Auth Controller - Login", function () {
     });
 
     User.findOne.restore();
+  });
+
+  it("should send a response with a valid user status for an existing user", function (done) {
+    mongoose
+      .connect(
+        "mongodb+srv://Maximilian-Nodejs-MS:Southkorea121@cluster0.llotg6f.mongodb.net/test-messages?retryWrites=true&w=majority",
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        }
+      )
+      .then((result) => {
+        // app.listen(8080);
+        // console.log("Connected to Server successfully!");
+        const user = new User({
+          email: "test@test.com",
+          password: "123456",
+          name: "Test",
+          posts: [],
+        });
+        return user.save();
+      })
+      .then(() => {
+        
+      })
+      .catch((err) => console.log(err));
   });
 });
