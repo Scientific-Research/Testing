98c710f1c45cb7f70362984cf35c1e5af50c1037
Maximilian <scientificresearch309@gmail.com>
2023-12-12 13:05:51 +0100

    this test: should throw an error if the authorization header is only one string passed!

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -12,3 +12,12 @@ it("should throw an error if no authorization header is present!", function () {
     "Not authenticated."
   );
 });
+
+it("should throw an error if the authorization header is only one string", function () {
+  const req = {
+    get: function (headerName) {
+      return "xyz";
+    },
+  };
+  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+});
