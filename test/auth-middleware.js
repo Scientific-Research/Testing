b483e9449dbf68619043be05dadea6c4ac68f0c8
Maximilian <scientificresearch309@gmail.com>
2023-12-12 18:36:38 +0100

    Using Stubs - installing sinon

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,4 +1,6 @@
 const expect = require("chai").expect;
+const jwt = require("jsonwebtoken");
+const sinon = require("sinon");
 
 const authMiddleware = require("../middleware/is-auth");
 
@@ -23,6 +25,25 @@ describe("Auth middleware", function () {
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
   });
 
+  it("should yield a userId after decoding the token", function () {
+    const req = {
+      get: function (headerName) {
+        return "Bearer klhdfgkjdhfgjdfgdddjsdklfgj";
+      },
+    };
+    sinon.stub(jwt, "verify");
+    jwt.verify.returns({ userId: "abc" });
+    // jwt.verify = function () {
+    //   return { userId: "abc" };
+    // };
+    // expect(authMiddleware.bind(this, req.userId, {}, () => {})).to.throw();
+    authMiddleware(req, {}, () => {});
+    expect(req).to.have.property("userId");
+    expect(req).to.have.property("userId", "abc");
+    expect(jwt.verify.called).to.be.true;
+    jwt.verify.restore();
+  });
+
   it("should throw an error if the token cannot be verified", function () {
     const req = {
       get: function (headerName) {
@@ -30,15 +51,5 @@ describe("Auth middleware", function () {
       },
     };
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
-  });
-  it("should yield a userId after decoding the token", function () {
-    const req = {
-      get: function (headerName) {
-        return "Bearer klhdfgkjdhfgjdfgjsdklfgj";
-      },
-    };
-    // expect(authMiddleware.bind(this, req.userId, {}, () => {})).to.throw();
-    authMiddleware(req, {}, () => {});
-    expect(req).to.have.property("userId");
   });
 });
